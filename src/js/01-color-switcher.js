878c94155ea222aec7fee73f74c3e4175baeefb4
OleksandrTymoshenko <nomakievip@gmail.com>
2021-12-29 17:44:21 +0200

    src v-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+let timeId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]')
+};
+
+refs.btnStart.addEventListener('click', hendleChangeBodyColor);
+
+function hendleChangeBodyColor(e) {
+    refs.btnStop.style.backgroundColor = 'red';
+    refs.btnStart.style.backgroundColor = '';
+    timeId = setInterval(() => {
+        console.log(timeId);
+        document.body.style.backgroundColor = getRandomHexColor();
+}, 1000);
+    if (timeId) {
+        refs.btnStart.disabled = true;
+refs.btnStop.disabled = false;
+    }
+}
+
+refs.btnStop.addEventListener('click', hendlerStopChangeBodyColor);
+
+function hendlerStopChangeBodyColor(e) {
+    clearInterval(timeId);
+    refs.btnStart.disabled = false;
+    refs.btnStart.style.backgroundColor = 'green';
+    refs.btnStop.disabled = true;
+refs.btnStop.style.backgroundColor = '';
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  let delays = Number(delay.value);
+  let steps = Number(step.value);
+  let amounts = Number(amount.value);
+  let position = 1;
+
+  if (delays < 0 || steps < 0) {
+    Notify.failure(`FirsDelay end Delay step - CANNOT BE NEGATIVE`);
+    return;
   }
+
+  for (position; position <= amounts; position += 1) {
+    createPromise(position, delays)
+      .then(({ position, delays }) => {
+        setTimeout(() => {
+          Notify.success(`Fulfilled promise ${position} in ${delays}ms`);
+        }, delays);
+      })
+      .catch(({ position, delays }) => {
+        setTimeout(() => {
+          Notify.failure(`Rejected promise ${position} in ${delays}ms`);
+        }, delays);
+      });
+    delays += steps;
+  }
+}
+
+function createPromise(position, delays) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delays }); // Fulfill
+    } else {
+      reject({ position, delays }); // Reject
+    }
+  });
 }
